9f1247eb1e03fa1729b90417666f9db73d8f675e
Mehdi Anoosheh <med.anooshe@gmail.com>
2024-12-23 21:55:51 +0330

    register user refactor

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,46 +1,8 @@
 const express = require("express");
-const { saveUser, findUser } = require("../db/db");
-const User = require('../models/usermodel');
-const bcrypt = require('bcrypt');
-const mongoose = require("mongoose");
-const errorTemplate = require('../templates/errorTemplate');
 const router = express.Router();
-const loginUser = require('../services/userService');
+const { loginUser, registerUser } = require('../services/userService');
 
-router.post("/register", (req, res, next) => {
-    findUser({ email: req.body.email })
-        .then((user) => {
-            if (user) {
-                return res.status(409).json({ message: "User already exists!" });
-            }
-            else {
-                const user = new User();
-                user._id = new mongoose.Types.ObjectId();
-                const newUser = Object.assign(user, req.body);
-                bcrypt.hash(newUser.passWord, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(501).json({ message: "Error " + err.message });
-                    }
-                    else {
-                        newUser.passWord = hash;
-                        saveUser(newUser)
-                            .then((user) => {
-                                return res.status(201)
-                                    .json({ message: "Successful registration", user: user });
-                            })
-                            .catch((err) => {
-                                errorTemplate(res, err);
-                            });
-
-                    }
-                });
-            }
-        })
-        .catch((err) => {
-            errorTemplate(res, err);
-        });
-
-});
+router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -1,8 +1,45 @@
 require('dotenv').config();
-const { findUser } = require("../db/db");
+const User = require('../models/usermodel');
+const mongoose = require("mongoose");
+const { saveUser, findUser } = require("../db/db");
 const errorTemplate = require("../templates/errorTemplate");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const registerUser = async (req, res) => {
+    findUser({ email: req.body.email })
+        .then((user) => {
+            if (user) {
+                return res.status(409).json({ message: "User already exists!" });
+            }
+            else {
+                const user = new User();
+                user._id = new mongoose.Types.ObjectId();
+                const newUser = Object.assign(user, req.body);
+                console.log(newUser);
+                bcrypt.hash(newUser.passWord, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(501).json({ message: "Error " + err.message });
+                    }
+                    else {
+                        newUser.passWord = hash;
+                        saveUser(newUser)
+                            .then((user) => {
+                                return res.status(201)
+                                    .json({ message: "Successful registration", user: user });
+                            })
+                            .catch((err) => {
+                                errorTemplate(res, err);
+                            });
+
+                    }
+                });
+            }
+        })
+        .catch((err) => {
+            console.log('err');
+            errorTemplate(res, err);
+        });
+};
 
 const loginUser = async (req, res) => {
     try {
@@ -31,4 +68,4 @@ const loginUser = async (req, res) => {
     }
 };
 
-module.exports = loginUser;
+module.exports = { loginUser, registerUser };
